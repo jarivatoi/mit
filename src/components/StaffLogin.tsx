ae0340a9ecb0f565d50a8a5ccae26b468a794493
jarivatoi <81554724+jarivatoi@users.noreply.github.com>
2026-05-28 05:25:51 +0400

    Updated StaffLogin.tsx

--- src/components/StaffLogin.tsx
+++ src/components/StaffLogin.tsx
@@ -371,7 +371,7 @@ const StaffLogin: React.FC<StaffLoginProps> = ({ onLoginSuccess, onRegister, sho
       
       if (rec.error || !rec.data) throw new Error('User not found')
       const row = rec.data
-      if (!row.is_active) throw new Error('Access Denied')
+      if (!row.is_active) throw new Error('User is inactive')
       
       const hashed = await hash(passcode)
       console.log('Login attempt:', { passcode, hashed, storedHash: row.passcode_hash });
